d7039e177ebd4667577cda6bca7a37b0f7ab69a8
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2024-05-03 23:50:02 +0300

    Extend erc721 mock for testing purposes

--- contracts/mocks/ERC721Mock.sol
+++ contracts/mocks/ERC721Mock.sol
@@ -3,12 +3,22 @@ pragma solidity 0.8.25;
 
 // Imports
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract ERC721Mock is ERC721 {
+contract ERC721Mock is ERC721, ERC721URIStorage {
 
     constructor() ERC721("ERC721Mock", "NFTMOCK") { }
 
-    function mint(address _to, uint256 _tokenId) external {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function mint(address _to, uint256 _tokenId, string memory _tokenURI) external {
         _safeMint(_to, _tokenId);
+        _setTokenURI(_tokenId, _tokenURI);
     }
 }
