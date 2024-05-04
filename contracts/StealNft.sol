0c8c4652e9971759a90e796ba2037a16a7afa570
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2024-05-04 12:49:11 +0300

    Write happy path test

--- contracts/StealNft.sol
+++ contracts/StealNft.sol
@@ -27,20 +27,6 @@ contract StealNft is ERC721, ERC721URIStorage {
     // Functions
 
     /**
-     * @dev Fetches the metadata of an NFT contained in a foreign ERC721 collection and returns it as a string.
-     *
-     * Requirements:
-     *
-     * - `nftContractAddress` has to be a valid ERC721 implementation.
-     * - `tokenId` has to be a valid token id for the given ERC721.
-     */
-    function fetchNftMetadataToSteal(address nftContractAddress, uint256 tokenId) public view returns (string memory tokenMetadata) {
-        IERC721Metadata foreignNftContract = IERC721Metadata(nftContractAddress);
-
-        return foreignNftContract.tokenURI(tokenId);
-    }
-
-    /**
      * @dev Mints in this collection an NFT with the same metadata as the one given in the foreign ERC721 collection.
      *
      * Requirements:
@@ -49,7 +35,7 @@ contract StealNft is ERC721, ERC721URIStorage {
      * - `tokenId` has to be a valid token id for the given ERC721.
      */
     function steal(address nftContractAddress, uint256 tokenId, address receiverAddress) public {
-        string memory metadataUri = fetchNftMetadataToSteal(nftContractAddress, tokenId);
+        string memory metadataUri = IERC721Metadata(nftContractAddress).tokenURI(tokenId);
 
         _safeMint(receiverAddress, ++_nextTokenId);
         _setTokenURI(tokenId, metadataUri);
--- test/StealNft.js
+++ test/StealNft.js
@@ -1,15 +1,16 @@
-const { expect } = require('chai')
-const { ethers } = require('hardhat')
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('StealNft', () => {
 
-    // Variables
-	let erc721Mock
-    let stealNft
+    const originTokenId = 1;
 
-	let pleb
-	let chad1
-	let chad2
+	let erc721Mock;
+    let stealNft;
+
+	let pleb;
+	let chad1;
+	let chad2;
 
     beforeEach(async () => {
         const ERC721Mock = await ethers.getContractFactory('ERC721Mock');
@@ -19,10 +20,14 @@ describe('StealNft', () => {
         stealNft = await StealNft.deploy();
 
         [pleb, chad1, chad2] = await ethers.getSigners();
-        erc721Mock.mint(pleb.address, 0);
-    })
+        await erc721Mock.mint(pleb.address, originTokenId, "ipfs://something-legally-owned-by-pleb");
+    });
 
-    it('Should claim NFT', async () => {
-		// await erc721Mock._safeMint(owner.address, 0, "");
-    })
-})
+    it('Should mint NFT in StealNFT contract with the same metadata as the original one', async () => {
+        const originTokenMetadata = await erc721Mock.tokenURI(originTokenId);
+        await stealNft.connect(chad1).steal(await erc721Mock.getAddress(), originTokenId, chad1.address);
+        const stolenTokenMetadata = await stealNft.tokenURI(1);
+
+        expect(stolenTokenMetadata).to.equal(originTokenMetadata);
+    });
+});
